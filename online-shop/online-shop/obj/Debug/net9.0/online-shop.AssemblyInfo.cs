c2c2b21b019f5259bc7cf445b601994f77207168
Denis Savin <ddenis22072006@gmail.com>
2026-07-31 22:38:32 +0300

    feat: added 8 blocks for golang

--- online-shop/online-shop/obj/Debug/net9.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net9.0/online-shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f370fff7817794566f804b92aed69a63bb1d28d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6416a55d2aa5b4377ab95f88ae4f5c01da2b3a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
