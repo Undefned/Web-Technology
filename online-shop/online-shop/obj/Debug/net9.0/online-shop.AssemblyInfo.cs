3a36aca011ff5735ba0526ef3ce4351594e78c9e
Denis Savin <ddenis22072006@gmail.com>
2026-08-04 06:26:15 +0300

    feat(PopularQuestionsForSE): added 49 gogin and java blocks

--- online-shop/online-shop/obj/Debug/net9.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net9.0/online-shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c2b21b019f5259bc7cf445b601994f77207168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e467d0d6ba3eed7f261ef2074d53876215377a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
